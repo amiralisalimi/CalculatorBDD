acd76403fb8ab38521a929bed01463346210bbec
A.Aminianfar <aminianfar.amir@gmail.com>
2025-08-15 11:09:08 +0330

    add main calculator code based on tests

--- src/main/java/edu/sharif/selab/calculator/Calculator.java
+++ src/main/java/edu/sharif/selab/calculator/Calculator.java
@@ -2,6 +2,20 @@ package edu.sharif.selab.calculator;
 
 public class Calculator {
     public double calculate(double num1, double num2, char op) {
-        return 0;
+        switch (op) {
+            case '+':
+                return num1 + num2;
+            case '-':
+                return num1 - num2;
+            case '*':
+                return num1 * num2;
+            case '/':
+                if (num2 == 0) {
+                    return Double.POSITIVE_INFINITY;
+                }
+                return (double) num1 / num2;
+            default:
+                throw new IllegalArgumentException("عملگر نامعتبر: " + op);
+        }
     }
 }
